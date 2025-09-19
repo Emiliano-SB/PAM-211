f32c8d8dc15e89b95c747a806065a519aa4ba2e9
Emiliano-SB <124051706@upq.edu.mx>
2025-09-18 20:47:57 -0600

    Ejercicio 3
    Realizar los dos anteriores en uno solo

--- Practica-1/Practica1.js
+++ Practica-1/Practica1.js
@@ -13,9 +13,21 @@ const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 console.log(saludo);
 
 //Ejercicio B
-
+// Lo primero que hice fue de cambiar la primera letra reservada de "function" a del tipo "var" ya que el ejercicio nos pide 
+//obtener el cuadrado de 3 número diferentes, además de eso lo que hice fue agregar un "=" antes del parametro del número al calcular para que después
+//del "(numero)" ahora si agregar la flecha del "arrow function" y eliminar los corchetes y el return para que solo quede la operación del cuadrado del número
+//y por ultimo fue agregar los "console.log" para que muestre el resultado de los 3 números.
 var cuadrado = (numero) => numero * numero;
 console.log(cuadrado(5)); 
 console.log(cuadrado(2));
 console.log(cuadrado(3));
+
+//Ejercicio C
+//Fue ejercicio de juntando los dos códigos anteriores 
+const saludoPersonalizado = (nombre, edad) => {
+    return "Hola, me llamo " +nombre+  " y tengo " +edad+ " años.";
+};
+
+const mensaje = saludoPersonalizado("Emiliano", 20);
+console.log(mensaje); 
 
