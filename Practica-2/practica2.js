17b9163aec296ddf268bbe50447d9587c92c9924
Emiliano-SB <124051706@upq.edu.mx>
2025-09-22 19:36:41 -0600

    Ejercicio B:
    Ahora lo que hice en este ejercicio fue hacer  dos métodos para filtrar los datos del array que nos dio el ejercicio, el primer filtro fue solo apartar los productos que tienen precio mayor a $1000 y con base a esos productos crear otro arreglo para que con estos productos ya filtrados solo mostrar un nuevo arreglo con los nombres de dichos productos

--- Practica-2/practica2.js
+++ Practica-2/practica2.js
@@ -14,3 +14,19 @@ const { nombre, edad, direccion: { ciudad, pais } } = persona;
 //Ahora lo que hice aqui fue ya imprimir en consola lo que es el saludo del ejercicio que nos pide con todos los datos
 //que se nos dieron dentro del ejercicio que es mostrando el nombre de la persona, su edad y la ciudad en la que vive el usuario
 console.log("Hola, me llamo " + persona.nombre + ", tengo " + persona.edad + " años y vivo en " +persona.direccion.ciudad+ ".");
+
+//Ejercicio B
+const productos=[
+    {nombre: "Laptop", precio: 12000},
+    {nombre: "Mouse", precio: 250},
+    {nombre: "Teclado", precio: 750},
+    {nombre: "Monitor", precio: 3000},
+];
+//Aquí lo que hice fue lo que es hacer método para filtar el arreglo "productos" para que el primer filtro (método filter) solo
+//filtre del arreglo para solo quedarme con los productos que sea su precio mayor a $1000 
+const productosfiltrados = productos.filter(producto => producto.precio > 1000);
+//Y ahora lo que hice fue el segundo filtro (método map) para que después de filtrar los productos solo me regrese los 
+// nombre de dichos productos que se pide en el primer filtro
+const nombres = productosfiltrados.map(productos => productos.nombre);
+console.log(nombres);
+
