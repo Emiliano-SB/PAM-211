fc462bffc5ecc837deb1555c602462096a08abd0
Emiliano-SB <124051706@upq.edu.mx>
2025-09-25 23:22:33 -0600

    Ejercicio C:
    Lo que hice en este ejercicio fue un poco acerca de la creación (solo completar lo que faltaba) de una async para crear y ayudar a la simulación de una función en donde se ocupa un await ya que dentro del ejercicio se solicito un poco de simulacion de peticiones a una API en donde en la función de "simularPeticionAPI" se tiene un  setTimeout de 5 segundos donde después de que se mande a llamar se logre mostrar el mensaje que se tiene en esta funcion

--- Practica-3/Practica3.js
+++ Practica-3/Practica3.js
@@ -4,7 +4,7 @@
 function verificarUsuario(usuario)
 {
     return new Promise((resolve, reject) => {
-        if(acceso && usuario === "admin")
+        if(usuario === "admin")
         {
             resolve("Acceso concedido");
         }
@@ -24,3 +24,23 @@ verificarUsuario("admin")
 verificarUsuario("Ivan")
     .then(res => console.log(res))
     .catch(err => console.log(err));
+
+//Ejercicio C
+function simularPeticionAPI(){
+    return new Promise(resolve =>{
+        setTimeout(() => {
+            resolve("Datos recibidos correctamente");
+        }, 5000);
+    });
+}
+//Lo que hice fue crear una función asíncrona (Async function) en donde le defini la variable "datos" 
+//la cuál va a esperar con un "await" el tiempo que se declaro en la promesa de la función "simularPeticionAPI" para después
+//imprimir el mensaje en la consola
+async function obtenerDatos(){
+    let datos = await simularPeticionAPI();
+    console.log(datos);
+}
+//Estos datos como tal los agregue para que se note un poco "real" la petición que se me pide en el ejercicio
+console.log("Iniciando petición a la API...");
+obtenerDatos(); //Aquí se llama a la función de "obtenerDatos" para que empiece a ejecutarse las funciones anteriores 
+console.log("Petición en proceso...");
