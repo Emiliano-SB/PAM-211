2ca86de37d77292fdfffaf37fdf4d6a8dd328a33
Emiliano-SB <124051706@upq.edu.mx>
2025-10-07 17:04:40 -0600

    Practica 5:
    Dentro de esta practica lo que realizamos es un poco acerca de las funcionalidades de los componentes Props y State, además de poder utilizar el proyecto creado de la clase pasada para poder entender este tema

--- INTRO/App.js
+++ INTRO/App.js
@@ -1,15 +1,29 @@
+
+//1. Imports: Zona de importaciones a las screens (pantallas)
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react'; //Importamos React y useState ya que lo ocuparemos más adelante
+import { Button } from 'react-native-web';
 
+//2. Main: Zona de componentes (trabajaremos aquí principalmente)
 export default function App() {
+  //     variable | función
+  const [contador, setContador]= useState(0); //Hacemos una funcionalidad del boton para que vaya aumentando el contador con los clics
   return (
-    <View style={styles.container}>
-      <Text> Hola Mundo React Native!!</Text>
+    <View style={styles.container}> 
+
+      <Text> Contador: {contador}</Text>
+      <Button title='Agregar' onPress={()=> setContador(contador + 1)}/>
+      <Button title='Quitar' onPress={()=> setContador(contador - 1)}/>
+      <Button title='Reiniciar' onPress={()=> setContador(contador *0)}/>
       <StatusBar style="auto" />
+
     </View>
+
   );
 }
 
+//3. Estilos: Zona de estilos para los componentes
 const styles = StyleSheet.create({
   container: {
     flex: 1,
