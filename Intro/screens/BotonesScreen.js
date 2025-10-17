55bdd9841aca875713b26b010f5d6c48753e6bf4
Emiliano-SB <124051706@upq.edu.mx>
2025-10-16 20:18:21 -0600

    Practica Botones y Switch
    En esta practica lo que hicimos fue un poco acerca de los funcionamientos de los botones y de los switches

--- Intro/screens/BotonesScreen.js
+++ Intro/screens/BotonesScreen.js
@@ -1,12 +1,83 @@
-import { Text, View } from 'react-native-web'
+import { Text, View, StyleSheet } from 'react-native-web'
+import React, {use, useState} from 'react'
+import { TouchableOpacity, Switch } from 'react-native';
 
 
 export default function BotonesScreen(){
+
+  const[prendido, setPrendido] = useState(false);
+  const backgroundColor = prendido ? '#f5f5f5' : '#121212' ;
+  const textColor = prendido ? '#000' : '#FFF';
   
     return (
-      <View>
-        <Text>Proximamente..............</Text>
+      <View style= {[styles.container, {backgroundColor}]}>
+
+        <Text style={[styles.texto, {color: textColor}]}> Estado: {prendido ? 'prendido' : 'apagado'}</Text>
+
+        <TouchableOpacity 
+        style={styles.botonEncender} 
+        onPress={()=>setPrendido(true)}> 
+        <Text style={styles.textoBoton}>Prender</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+        style={styles.botonApagar} 
+        onPress={()=>setPrendido(false)}> 
+        <Text style={styles.textoBoton}>Apagar</Text>
+        </TouchableOpacity>
+
+        <View style={styles.switchContainer}>
+
+          <Text style={[styles.switchLabel, {color: textColor}]}>Control de Switch: </Text>
+          <Switch value = {prendido} onValueChange={setPrendido}></Switch>
+
+        </View>
+
       </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1,
+    backgroundColor: '#836969ff', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  },
+  texto:{
+    color:'#07060aff',
+    fontSize: 20, 
+    marginBottom: 30,
+    fontFamily: 'Times New Roman', 
+    fontWeight: 'bold', 
+    fontStyle: 'Italic', 
+  },
+  botonEncender:{
+    backgroundColor:'#4caf50',
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    borderRadius: 8, 
+    marginBottom: 20,
+  },
+  botonApagar:{
+    backgroundColor:'#f44336',
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    borderRadius: 8, 
+    marginBottom: 20,
+  },
+  textoBoton:{
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  switchContainer:{
+    flexDirection: 'row',
+    alingItems: 'center',
+  },
+  switchLabel:{
+    fontSize: 16,
+    marginRight: 10,
+  },
+})
--- Intro/screens/TextoScreen.js
+++ Intro/screens/TextoScreen.js
@@ -1,12 +1,28 @@
-import { Text, View } from "react-native-web"
+import { StyleSheet, Text, View } from "react-native-web"
 
 
 export default function TextoScreen(){
   
     return (
-      <View>
-        <Text>Proximamente.......</Text>
+      <View style= {styles.container}>
+        <Text style={styles.texto}>Proximamente.......</Text>
       </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    backgroundColor: '#836969ff', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  },
+  texto:{
+    color:'#07060aff',
+    fontSize: 30, 
+    fontFamily: 'Times New Roman', 
+    fontWeight: 'bold', 
+    fontStyle: 'Italic', 
+  },
+})
