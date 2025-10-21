1ab0cb9a3cc7015c3918ed96cd396175b09644df
Emiliano-SB <124051706@upq.edu.mx>
2025-10-20 19:18:59 -0600

    Practica TextInput & Alert
    Lo que realizamos dentro de esta practica fue un poco la funcionalidad de nuestros componentes de TextInput (cuadros de texto) y las alerts.
    Tambien un poco acerca de crear un cuadro donde "censure" la contraseña y donde pongas ciertas lineas del texto

--- Intro/screens/TextoScreen.js
+++ Intro/screens/TextoScreen.js
@@ -1,11 +1,66 @@
-import { StyleSheet, Text, View } from "react-native-web"
+import { StyleSheet, Text, View, Button, Alert, TextInput, Platform } from "react-native"
+import React, {useState} from 'react'
 
 
 export default function TextoScreen(){
-  
+
+  const [nombre, setNombre] = useState('');
+  const [contrasena, setContrasena] = useState(''); //Estado para el de contraseña
+  const [texto, setTexto] = useState(''); //Estado para el de multilinea
+
+  const mostrarAlerta = () => {
+    if(nombre.trim() === '' || contrasena.trim() === '' || texto.trim() === ''){
+      if(Platform.OS === 'web'){
+        window.alert('Error: Por favor complete todos los campos');
+      }
+      else{
+        Alert.alert('Error: Por favor complete todos los campos');
+      }
+    } 
+    else{
+      if(Platform.OS === 'web'){
+        window.alert(`Hola ${nombre}`);
+      }
+      else{
+        Alert.alert(`Hola ${nombre}`);
+      }
+    }
+  }
+
     return (
       <View style= {styles.container}>
-        <Text style={styles.texto}>Proximamente.......</Text>
+
+        <Text style={styles.titulo}>Practica TextInput & Alert</Text>
+
+        <TextInput 
+        placeholder = 'Escribe tu nombre' 
+        value = {nombre} 
+        onChangeText={setNombre}
+        maxLength={50}
+        style={styles.recuadro}>
+        </TextInput>
+
+        <TextInput
+        placeholder ='Escribe tu contraseña' 
+        value = {contrasena} 
+        onChangeText={setContrasena}
+        secureTextEntry={true}
+        maxLength={20}
+        style={styles.recuadro}
+        keyboardType="numeric">
+        </TextInput>
+
+        <TextInput
+        placeholder='Escribe tu texto (maximo 4 lineas)' 
+        value = {texto} 
+        onChangeText={setTexto}
+        multiline={true}
+        numberOfLines={4}
+        maxLength={200}
+        style={styles.recuadro}>
+        </TextInput>
+
+        <Button color= 'brown' title='Mostrar Saludo' onPress={mostrarAlerta}></Button>
       </View>
     )
 
@@ -14,15 +69,20 @@ export default function TextoScreen(){
 const styles = StyleSheet.create({
   container: { 
     flex: 1, 
-    backgroundColor: '#836969ff', 
+    backgroundColor: '#716363ff', 
     alignItems: 'center', 
     justifyContent: 'center', 
+    padding: 20,
   },
-  texto:{
-    color:'#07060aff',
-    fontSize: 30, 
-    fontFamily: 'Times New Roman', 
-    fontWeight: 'bold', 
-    fontStyle: 'Italic', 
+  titulo:{
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 15,
   },
-})
+  recuadro:{
+    borderWidth: 1,
+    bordeColor: '#000000ee',
+    padding: 10,
+    marginBottom: 20,
+  },
+});
