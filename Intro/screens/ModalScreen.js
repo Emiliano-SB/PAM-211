eac8942c9c7750dec8e84090ddd74fc3f6c1bc80
Emiliano-SB <124051706@upq.edu.mx>
2025-11-10 18:08:01 -0600

    Práctica Modal
    En esta practica lo que hicimos fue aprender el uso de un modal que en este caso es como otro tipo de ventana emergente pero con animación

--- Intro/screens/ModalScreen.js
+++ Intro/screens/ModalScreen.js
@@ -1,12 +1,61 @@
-import { Text, View } from "react-native-web"
+import { Text, StyleSheet, View, Button, Modal } from 'react-native'
+import { useState } from 'react'
 
 
 export default function ModalScreen(){
   
-    return (
-      <View>
-        <Text>Proximamente.......</Text>
-      </View>
-    )
+  const [modalVisible, setModalVisible] = useState(false);
 
+  return (
+    <View style={styles.container}>
+
+      <Button title='Mostrar modal' onPress={()=> setModalVisible(true)} color='black'>
+        Picame
+      </Button>
+
+      <Modal
+      animationType='fade'
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(!modalVisible)}>
+
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.textoModal}>Soy un modal</Text>
+            <Button title='Ocultar Modal' onPress={()=> setModalVisible(false)} color='black'></Button>
+          </View>
+        </View>
+
+      </Modal>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#ffffffe3',
+    alignItems:'center',
+    justifyContent:'center', 
+  },
+  modalContainer:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#b68cd134'
+  },
+  modalContent:{
+    backgroundColor:'#c3a1e9ff',
+    padding: 25,
+    borderRadius: 15,
+    alignItems:'center',
+  },
+  textoModal:{
+    color: '#000000',
+    fontSize:30,
+    fontFamily: 'Times New Roman',
+    fontWeight:'bold',
+    fontStyle:'italic',
+    marginBottom: 15,
+  }
+})
