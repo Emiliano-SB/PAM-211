8d2f9d8367b228a15e435ca6d432f9828f5f0379
Emiliano-SB <124051706@upq.edu.mx>
2025-11-24 19:26:25 -0600

    Insert y Select funcionando

---
+++ userDB/controllers/UsuarioController.js
@@ -0,0 +1,57 @@
+import { Usuario } from '../models/usuario.js';
+import DatabaseService from '../database/DatabaseService.js';
+
+export class UsuarioController{
+    constructor(){
+        this.listeners = [];
+    }
+
+    //Inicializar el controlador con el Service
+    async initialize(){
+        await DatabaseService.initialize();
+    }
+
+    //Contolador para invocar el servicio de consulta cuando se le indique
+    async obtenerUsuarios(){
+        try {
+            const data = await DatabaseService.getAll();
+            return data.map( u => new Usuario(u.id, u.nombre, u.fecha_creacion));
+        } catch (error) {
+
+        }
+    }
+
+    //funcion para crear usuario la cual primero valida 
+    async crearUsuario(nombre){
+        try {
+            //1. Validar datos
+            Usuario.validar(nombre);
+
+            //2. Insertar en BD
+            const nuevoUsuario = await DatabaseService.add(nombre.trim());
+
+            //3. Notificar usuario creado
+            return new Usuario(
+                nuevoUsuario.id,
+                nuevoUsuario.nombre,
+                nuevoUsuario.fecha_creacion
+            );
+        } catch(error) {
+            console.error('Error al crear usuario:', error);
+            throw error;
+        }
+    }
+
+    //Sistema de observadores para actualizar la vista automáticamente
+    addListener(callback){
+        this.listeners.push(callback);
+    }
+
+    removeListener(callback){
+        this.listeners = this.listeners.filter( l => l !== callback);
+    }
+
+    notifyListeners(){
+        this.listeners.forEach( callback => callback());
+    }
+}
